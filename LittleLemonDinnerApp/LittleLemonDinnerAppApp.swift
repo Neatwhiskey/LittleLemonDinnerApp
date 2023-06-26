875691a59c54c141c0408ca04d91af3b4052807a
Neatwhiskeyy <neatwhiskey24@gmail.com>
2023-06-26 17:11:42 +0100

    Initial Commit

---
+++ LittleLemonDinnerApp/LittleLemonDinnerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonDinnerAppApp.swift
+//  LittleLemonDinnerApp
+//
+//  Created by Jamaaldeen Opasina on 26/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonDinnerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
